04107c987fa9879ca0969e864d3b8a90ed835636
Dominick Ricardo Cifuentes Tomas <dominickt1703@gmail.com>
2026-08-05 00:54:18 -0600

    feat: agregar primera versión funcional del analizador léxico

---
+++ samples/ejemplo_basico.hs
@@ -0,0 +1,9 @@
+module Principal where
+
+-- Programa básico para probar el analizador
+edad = 20
+nombre = "Dominick"
+
+esAdulto = if edad >= 18
+           then "Mayor de edad"
+           else "Menor de edad"
