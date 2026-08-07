5bad103d43d15401dc4eed4c56ff888643cdb34f
Yeisson Poroj Toc <yeissonalexander07@gmail.com>
2026-08-07 10:37:19 -0600

    feat: agregar selector de archivos Haskell

--- gui/app.py
+++ gui/app.py
@@ -1,12 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
-
+from tkinter import filedialog
 
 class AnalizadorGUI:
+
+    def abrir_archivo(self):
+        archivo = filedialog.askopenfilename(
+            title="Seleccionar archivo Haskell",
+
+            filetypes=[
+                ("Archivos Haskell","*.hs"),
+                ("Todos los archivos","*.*")
+                ]
+    
+        )
+        if archivo:
+            self.archivo_actual = archivo
+            self.estado.config(
+                text=f"Estado: Archivo seleccionado"
+            )
 
     def __init__(self, root):
 
         self.root = root
+        self.archivo_actual = ""
 
         self.root.title("Analizador Lexico de Haskell")
         self.root.geometry("1100x700")
@@ -30,24 +47,28 @@ class AnalizadorGUI:
         tk.Button(
             botones,
             text="Abrir archivo",
-            width=18
+            width=18,
+            command=self.abrir_archivo
         ).pack(side=tk.LEFT, padx=10)
 
         tk.Button(
             botones,
             text="Analizar código",
-            width=18
+            width=18,
         ).pack(side=tk.LEFT)
 
-        ttk.Separator(self.root, orient="horizontal").pack(fill="x", pady=15)
+        ttk.Separator(
+            self.root, 
+            orient="horizontal"
+            ).pack(fill="x", pady=15)
 
-        estado = tk.Label(
+        self.estado = tk.Label(
             self.root,
             text="Estado: Esperando archivo...",
             anchor="w"
         )
 
-        estado.pack(fill="x", padx=20)
+        self.estado.pack(fill="x", padx=20)
 
 if __name__ == "__main__":
 
